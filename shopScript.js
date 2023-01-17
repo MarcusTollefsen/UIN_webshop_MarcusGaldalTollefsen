51ead5fe0658e6c428bc7530aaaae3f4c2776e89
MarcusTollefsen <113443191+MarcusTollefsen@users.noreply.github.com>
2023-01-17 15:52:05 +0100

    commit 4 😎

---
+++ shopScript.js
@@ -0,0 +1,26 @@
+let cart = []
+
+function addToCart(title, price){
+    cart.push({productTitle: title, productPrice: price, productQuantity: 1})
+    console.log(cart)
+    renderCart()
+
+    document.querySelector("#cart .label").innerHTML = cart.length
+}
+
+
+function renderCart(){
+    let listHTML = ""
+    cart.map(prod => listHTML += `<li>
+    <span class="title">${prod.productTitle}</span>
+    <span class="price">${prod.productPrice}</span>
+    <span class="quantity">${prod.productQuantity}</span>
+    <button class="delete">X</button>
+</li>`)
+
+    document.querySelector("#cartview ul").innerHTML = listHTML
+}
+
+function toggleCart(){
+    document.querySelector("#cartview").classList.toggle("hidden")
+}
